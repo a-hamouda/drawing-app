4e5f4c582146e9ace2d622237767735d5bef4074
joseph <abdulhamid0yusuf@gmail.com>
2022-08-31 22:16:55 +0200

    add: zoom canvas

--- src/core/sketch.js
+++ src/core/sketch.js
@@ -1,11 +1,15 @@
 const sketch = (canvasColor, canvasWidth, canvasHeight) => {
     let toolbox = null;
     let isSetup = false;
+    let zoomFactor = 1;
 
     return (canvas) => {
         canvas.setup = () => {
             const mainCanvas = canvas.createCanvas(canvasWidth, canvasHeight);
             mainCanvas.parent("sketchCanvas");
+
+            $(`#canvasZoom`).val(zoomFactor + "x");
+            canvas.setupZoomHandlers();
 
             new SketchActions(canvas, canvasColor);
             //create a toolbox for storing the tools
@@ -30,6 +34,30 @@ const sketch = (canvasColor, canvasWidth, canvasHeight) => {
         canvas.mouseMoved = () => canvas.trackMouseOverCanvas();
         canvas.mouseDragged = () => canvas.trackMouseOverCanvas();
 
+        canvas.setupZoomHandlers = () => {
+            $(`#canvasIncreaseZoom`).on("click", () => {
+                if (zoomFactor >= 3) return;
+                zoomFactor += 0.5;
+                canvas.zoom();
+            });
+
+            $(`#canvasDecreaseZoom`).on("click", () => {
+                if (zoomFactor <= 0.5) return;
+                zoomFactor -= 0.5;
+                canvas.zoom();
+            });
+        };
+
+        canvas.zoom = () => {
+            // set zoom input value on canvas info pane
+            $(`#canvasZoom`).val(zoomFactor + "x");
+            const tempCanvas = canvas.createGraphics(canvasWidth, canvasHeight);
+            tempCanvas.image(canvas, 0, 0, canvasWidth, canvasHeight);
+            tempCanvas.loadPixels();
+            canvas.resizeCanvas(canvasWidth * zoomFactor, canvasHeight * zoomFactor);
+            canvas.image(tempCanvas, 0, 0, canvasWidth * zoomFactor, canvasHeight * zoomFactor);
+        };
+
         canvas.trackMouseOverCanvas = () => {
             const dx = canvas.mouseX;
             const width = canvas.width;
@@ -49,6 +77,6 @@ const sketch = (canvasColor, canvasWidth, canvasHeight) => {
             } else {
                 dyText.text(Math.round(dy) + "px");
             }
-        }
+        };
     };
 };
