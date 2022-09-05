a3c13683ca9502b5772c1a8a76cd1b26b950c3ef
joseph <abdulhamid0yusuf@gmail.com>
2022-09-05 11:28:23 +0200

    chore: add documentation for toolbox

--- src/core/toolbox.js
+++ src/core/toolbox.js
@@ -1,21 +1,30 @@
-//container object for storing the tools. Functions to add new tools and select a tool
+/**
+ * Painting tools container.
+ */
 class Toolbox {
     constructor(canvasBackgroundColor) {
         /**
+         * Paining tools.
          *
          * @type {Tool[]}
          */
         this.tools = [];
         /**
+         * Current active tool.
          *
          * @type {Tool}
          */
         this.selectedTool = null;
+        /**
+         * Background color of the canvas.
+         *
+         * @type {string}
+         */
         this.canvasBackgroundColor = canvasBackgroundColor;
     }
 
-    //add a tool to the tools array
     /**
+     * Add a painting tool.
      *
      * @param {Tool} tool
      */
@@ -23,7 +32,6 @@ class Toolbox {
         //check that the object tool has an icon and a name
         console.assert(tool.hasOwnProperty("icon") && tool.hasOwnProperty("name"),
             "make sure your tool has both a name and an icon");
-
         this.tools.push(tool);
         this.addToolIcon(tool.icon, tool.name);
         //if no tool is selected (i.e. none have been added so far)
@@ -33,15 +41,24 @@ class Toolbox {
         }
     }
 
-    //add a new tool icon to the html page
+    /**
+     * Add a new tool icon to the html page.
+     *
+     * @param {string} icon - icon image file path.
+     * @param {string} name - name of the tool.
+     */
     addToolIcon(icon, name) {
         const sideBarItem = $(`<img class="sideBarItem" width="42px" id=${name} alt="tool icon" src="${icon}">`);
         sideBarItem.on("click", () => this.toolbarItemClick(name));
         sideBarItem.appendTo($(`#toolbar`));
     };
 
+    /**
+     * Activate tool by name, and deactivate other tools.
+     *
+     * @param name - name of the tool.
+     */
     toolbarItemClick(name) {
-        //remove any existing borders
         const sideBarItems = $(`.sideBarItem`);
         for (const sideBarItem of sideBarItems) {
             $(sideBarItem).css("background-color", "transparent");
@@ -49,6 +66,11 @@ class Toolbox {
         this.selectTool(name);
     };
 
+    /**
+     * Select a tool by name.
+     *
+     * @param {string} toolName - name of the painting tool.
+     */
     selectTool(toolName) {
         //search through the tools for one that's name matches toolName
         for (let i = 0; i < this.tools.length; i++) {
@@ -65,6 +87,9 @@ class Toolbox {
         }
     }
 
+    /**
+     * Clear all drawing layers of all tools.
+     */
     clearDrawingLayers() {
         for (const tool of this.tools) {
             tool.clearDrawingLayer(this.canvasBackgroundColor);
