b9df3d03cec77fbb04816d28f2dc506fd1eb9fa9
joseph <abdulhamid0yusuf@gmail.com>
2022-09-05 12:37:43 +0200

    chore: add documentation to SprayCanTool

--- src/tools/sprayCanTool.js
+++ src/tools/sprayCanTool.js
@@ -1,10 +1,17 @@
+/**
+ * Circular spray tool.
+ */
 class SprayCanTool extends ToolWithOptions {
     /**
+     * Config of the particles.
+     *
      * @type {{weight: number, spread: number}}
      */
     #config;
 
     /**
+     * Spray color.
+     *
      * @type string
      */
     #strokeColor;
@@ -30,25 +37,40 @@ class SprayCanTool extends ToolWithOptions {
             this.drawingLayer.point(x, y);
             j++;
         }
-        // for (let i = 0; i < this.#config.weight; i++) {
-        //     this.drawingLayer.point(
-        //         this.canvas.random(mouse.x - this.#config.spread, mouse.x + this.#config.spread),
-        //         this.canvas.random(mouse.y - this.#config.spread, mouse.y + this.#config.spread)
-        //     );
-        // }
         this.drawingLayer.pop();
         this.hasChanges = true;
         super.updateCanvas();
     }
 
+    /**
+     * Check particle is inside a circle with the center being the mouse and the radius being
+     * half of the spray's spread value.
+     *
+     * @param {number} pointX - x-pos of the particle.
+     * @param {number} pointY - y-pos of the particle.
+     * @param {number} mouseX - x-pos of the mouse.
+     * @param {number} mouseY - y-pos of the mouse.
+     * @param {number} radius - circle radius.
+     * @return {boolean} - particle is inside circle.
+     */
     static #isInsideCircle(pointX, pointY, mouseX, mouseY, radius) {
         return Math.sqrt(Math.pow((pointX - mouseX), 2) + Math.pow((pointY - mouseY), 2)) < radius;
     }
 
+    /**
+     * Override spray color.
+     *
+     * @param {string} color - color of the spray.
+     */
     #onStrokeColorChanged(color) {
         this.#strokeColor = color;
     }
 
+    /**
+     * Override particles configuration.
+     *
+     * @param {{weight: number, spread: number}} config - particles configuration.
+     */
     #onConfigChanged(config) {
         this.#config = config;
     }
